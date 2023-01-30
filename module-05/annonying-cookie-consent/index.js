37649bdee5cfb25d92e4a6aadbc3455eda0e2ad2
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2023-01-30 12:48:54 +0200

    add Annoying Cookie Consent to module 05

---
+++ module-05/annonying-cookie-consent/index.js
@@ -0,0 +1,39 @@
+// global variables //
+const modal = document.getElementById('modal');
+const modalInner = document.getElementById('modal-inner');
+const modalForm = document.getElementById('modal-form');
+const modalText = document.getElementById('modal-text');
+
+// main //
+setTimeout(() => modal.style.display = 'inline', 1500);
+
+modalForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const modalFormData = new FormData(modalForm);
+    const fullName = modalFormData.get("userFullName");
+
+    modalForm.style.display = 'none';
+    
+    modalText.innerHTML = `
+    <div class="modal-loading">
+        <img class="modal-loading-img" src='./images/loading.svg' alt='animated loading'>
+        <p id="modal-loading-text">Uploading your data to the dark web...</p>
+    </div>
+    `;
+    setTimeout(() => {
+        document.getElementById('modal-loading-text').textContent = "Making the sale...";
+    }, 1500);
+
+    setTimeout(() => {
+        modalInner.innerHTML = `
+        <h2>Thanks <span class="modal-inner-name">${fullName}</span>, you Sucker!</h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif-container">
+            <img class="idiot-gif" src="./images/pirate.gif" alt="a pirate laughing">
+            
+        </div>
+        `;     
+    }, 3000);
+
+});
